c78620efca84e7e0fa9e2df7985596ada1cef649
carterjandorf <carter_jandorf@knights.ucf.edu>
2020-01-16 17:04:49 -0500

    first commit

---
+++ Jandorf02/src/Jandorf02/GoodbyeWorld.java
@@ -0,0 +1,12 @@
+package Jandorf02;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.printf("Goodbye, World!");
+		
+	}
+
+}
